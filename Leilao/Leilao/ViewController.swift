675f3d1511ceca07f6ad4a0b784e97228e476958
Michel dos Santos <michelsantos15@gmail.com>
2021-02-27 22:59:32 -0300

    add project leilao

---
+++ Leilao/Leilao/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Leilao
+//
+//  Created by Michel dos Santos on 27/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
